e34717f57be5115a9dd2df61ab85e71e3c971347
bluetoothfx <firebold42@gmail.com>
2024-12-14 11:11:39 +0600

    chore: update dependency

--- app/src/main/java/com/blueprint/composecrossdrill/di/RepositoryModules.kt
+++ app/src/main/java/com/blueprint/composecrossdrill/di/RepositoryModules.kt
@@ -1,8 +1,9 @@
 package com.blueprint.composecrossdrill.di
 
 import com.blueprint.composecrossdrill.data.repository.LoginRepositoryImpl
+import com.blueprint.composecrossdrill.domain.repository.LoginRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
-    factory {  LoginRepositoryImpl(get()) }
+    factory<LoginRepository> {  LoginRepositoryImpl(get()) }
 }
--- app/src/main/java/com/blueprint/composecrossdrill/ui/features/dashboard/screens/DashboardScreen.kt
+++ app/src/main/java/com/blueprint/composecrossdrill/ui/features/dashboard/screens/DashboardScreen.kt
@@ -37,6 +37,7 @@ fun DashboardScreen(
     LaunchedEffect(Lifecycle.State.STARTED) {
         val inputStream = context.resources.openRawResource(R.raw.data)
         dashboardViewModel.getUsers(inputStream)
+        dashboardViewModel.getUserTodos()
     }
 
     Scaffold(
