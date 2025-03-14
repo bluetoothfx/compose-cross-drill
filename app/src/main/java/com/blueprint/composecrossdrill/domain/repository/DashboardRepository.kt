3d2570fc7317e686a0527a9b820fb91a9fcfda87
Syed Refat Al Abrar <bluetoothfx@users.noreply.github.com>
2025-03-14 09:28:42 +0600

    feat: result wrapper for better management of data (#14)

--- app/src/main/java/com/blueprint/composecrossdrill/domain/repository/DashboardRepository.kt
+++ app/src/main/java/com/blueprint/composecrossdrill/domain/repository/DashboardRepository.kt
@@ -3,9 +3,10 @@ package com.blueprint.composecrossdrill.domain.repository
 import com.blueprint.composecrossdrill.domain.model.recipes.Recipe
 import com.blueprint.composecrossdrill.domain.model.recipes.RecipeUser
 import com.blueprint.composecrossdrill.domain.model.user.User
+import com.blueprint.composecrossdrill.utils.components.network.ResultWrapper
 
 interface DashboardRepository {
-    suspend fun getUsers(): List<User>
-    suspend fun getRecipes(): List<Recipe>
-    suspend fun getRecipesByUser(): List<RecipeUser>
+    suspend fun getUsers(): ResultWrapper<List<User>>
+    suspend fun getRecipes(): ResultWrapper<List<Recipe>>
+    suspend fun getRecipesByUser(): ResultWrapper<List<RecipeUser>>
 }
